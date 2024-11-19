1840b6ad74bc7a42c35c205ac36f2dfbb258a21f
SurajHande229 <139261755+SurajHande229@users.noreply.github.com>
2024-11-19 14:00:34 +0530

    Updated provider.tf

--- resource-group/provider.tf
+++ resource-group/provider.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.49.0"
-    }
-  }
+# Specify the Terraform provider
+provider "aws" {
+  region  = "us-east-1" # Change to your desired AWS region
+  profile = "default"   # AWS CLI profile (optional, remove if using default credentials)
 }
 
-provider "azurerm" {
-  features {
-    template_deployment {
-      delete_nested_items_during_deletion = true
+# Define required Terraform version
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Change version based on compatibility
     }
   }
+  required_version = ">= 1.4.0"
 }
