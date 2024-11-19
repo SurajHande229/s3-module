4f0f2bdcbfc9baa1883ad6debfcfd9f3afe8160f
Suraj Hande <Suraj.Hande@ezest.local>
2024-11-19 13:32:13 +0530

    uploaded ec2 code

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  #key_name      = var.key_name
+  #security_groups = [var.security_group]
+
+  tags = {
+    Name = "My-Terraform-EC2"
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+instance_type   = "t2.micro"
+ami_id          = "ami-012967cc5a8c9f891" # Replace with a valid AMI ID in your region
+#key_name        = "my-key-pair"          # Replace with your key pair
+#security_group  = "ami-012967cc5a8c9f891"  # Replace with your security group
